e0d2dc16eca6ec9adcf6facfdb6cabfc43567959
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-04 11:18:20 +0000

    s

--- Assignment 5 Algorithms and git/SEQUENCE.py
+++ Assignment 5 Algorithms and git/SEQUENCE.py
@@ -1,1 +1,11 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+for x in range(0, n):
+    sum = a + b + c
+    print(sum)
+    a = b
+    b = c
+    c = sum
