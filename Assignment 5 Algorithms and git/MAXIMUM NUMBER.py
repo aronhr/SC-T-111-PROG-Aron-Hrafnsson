0f99f6a7943587c7a60aab40bc268ae246389711
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-04 10:20:54 +0000

    Maximum Number complete

--- Assignment 5 Algorithms and git/MAXIMUM NUMBER.py
+++ Assignment 5 Algorithms and git/MAXIMUM NUMBER.py
@@ -1,8 +1,11 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
 
-# Búa til array sem tekur inn allar tölur sem notandi setur inn
+max_int = num_int
 
-# max_int = Math.Max(array)
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int > max_int:
+        max_int = num_int
 
 print("The maximum is", max_int)    # Do not change this line
