e7dbddafd4a287400b509ca8fd865b1497c26307
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-04 09:42:09 +0000

    Comments for Algorithim

---
+++ Assignment 5 Algorithms and git/MAXIMUM NUMBER.py
@@ -0,0 +1,8 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+# Búa til array sem tekur inn allar tölur sem notandi setur inn
+
+# max_int = Math.Max(array)
+
+print("The maximum is", max_int)    # Do not change this line
