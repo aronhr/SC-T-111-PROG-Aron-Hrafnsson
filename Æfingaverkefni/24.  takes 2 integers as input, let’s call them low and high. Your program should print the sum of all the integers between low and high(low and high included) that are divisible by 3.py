6f17cdd624bc003230f3a364953277a018ea30f8
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-10 23:03:43 +0000

    23 - 24

---
+++ Æfingaverkefni/24.  takes 2 integers as input, let’s call them low and high. Your program should print the sum of all the integers between low and high(low and high included) that are divisible by 3.py
@@ -0,0 +1,9 @@
+low = int(input("Enter lower number: "))
+high = int(input("Enter higher number: "))
+sums = 0
+
+for x in range(low, high + 1):
+    if x % 3 == 0:
+        sums = sums + x
+
+print(sums)
