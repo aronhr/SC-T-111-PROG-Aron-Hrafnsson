1eb35c4a508158601fe6d955341676fe41a56e29
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-10 21:37:26 +0000

    Refactor og dæmi 5,6

---
+++ Æfingaverkefni/5. 2 strings as input, let´s call them firstname and lastname. They should hold (as the names of them imply) the first and last name of the user.py
@@ -0,0 +1,6 @@
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+fullname = fname + " " + lname
+
+print("Your full name is " + fullname)
---
+++ Æfingaverkefni/6. nteger as input, lets call it celsius. This value will represent a temperature on the celsius scale.py
@@ -0,0 +1,3 @@
+celsius = int(input("Enter heat in celsius: "))
+
+print((celsius * 9/5) + 32)
