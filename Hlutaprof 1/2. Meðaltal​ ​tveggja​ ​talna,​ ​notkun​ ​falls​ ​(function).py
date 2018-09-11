e327e986a79870c2858f20ca25305fd3fb8ded06
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-11 02:42:14 +0000

    2

---
+++ Hlutaprof 1/2. Meðaltal​ ​tveggja​ ​talna,​ ​notkun​ ​falls​ ​(function).py
@@ -0,0 +1,8 @@
+def avg(f, l):
+    return (f + l) / 2
+
+
+a = float(input("Please enter a real number: "))
+b = float(input("Please enter another real number: "))
+
+print("The average of " + str(a) + " and " + str(b) + " is " + str(avg(a, b)))
