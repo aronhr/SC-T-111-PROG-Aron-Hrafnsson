1b15ba1db3e7214c0522382e3fd8634f1f5826c8
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-11 02:47:59 +0000

    4

---
+++ Hlutaprof 1/4. Leynisjálf.py
@@ -0,0 +1,8 @@
+i = input("Please input a character, ‘b’ to learn Batman’s secret identity, ‘r’ to learn Robins: ").lower()
+
+if i == "b":
+    print("Bjorgvin B")
+elif i == "r":
+    print("Kari H")
+else:
+    print("Have you ever danced with the devil in the pale moon light?")
