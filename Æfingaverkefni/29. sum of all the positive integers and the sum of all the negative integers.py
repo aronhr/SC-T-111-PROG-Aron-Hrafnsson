8775d2d8e38be86e59ef96bda715e4db4fb21e9e
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-11 00:18:47 +0000

    29

---
+++ Æfingaverkefni/29. sum of all the positive integers and the sum of all the negative integers.py
@@ -0,0 +1,15 @@
+turns = int(input("how many times: "))
+negative = []
+positive = []
+
+for x in range(1, turns + 1):
+    i = int(input("Enter a number: "))
+    if i < 0:
+        negative.append(i)
+    elif i > 0:
+        positive.append(i)
+
+print("Negative " + str(len(negative)))
+print("Sum of negative " + str(sum(negative)))
+print("Positive " + str(len(positive)))
+print("Sum of positive " + str(sum(positive)))
