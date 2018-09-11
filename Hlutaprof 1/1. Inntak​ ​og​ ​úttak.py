78aa3ef5f965bc0e34a35cc9f27aa7a1a790a18f
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-11 02:36:47 +0000

    1

---
+++ Hlutaprof 1/1. Inntak​ ​og​ ​úttak.py
@@ -0,0 +1,7 @@
+a = int(input("Please enter an integer: "))
+b = float(input("Please enter a real number: "))
+c = input("Please enter a character: ")
+
+print("Your integer: " + str(a))
+print("Your real number: " + str(b))
+print("Your character: " + c)
