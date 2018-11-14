393bc4a85f11b9e0b05eaf5046306600028fa1cb
Aron Hrafnsson <aronhr98@gmail.com>
2018-11-14 20:46:02 +0000

    Update Lokaverkefni/Hlutaprof 2/analyze list.py

--- Lokaverkefni/Hlutaprof 2/analyze list.py
+++ Lokaverkefni/Hlutaprof 2/analyze list.py
@@ -13,11 +13,6 @@ def is_prime(n):
 
 def print_list(listi):
     return [int(ss) for ss in listi]
-    listinn = []
-    for x in listi:
-        listinn.append(int(x))
-
-    return listinn
 
 
 def prime(listi):
