421f5dad4d78069684e61be5e90468d54f933e70
Aron Hrafnsson <aronhr98@gmail.com>
2018-11-14 19:10:49 +0000

    Update Lokaverkefni/Upprifjun/Upprifjunartími/daemi4.py

--- Lokaverkefni/Upprifjun/Upprifjunartími/daemi4.py
+++ Lokaverkefni/Upprifjun/Upprifjunartími/daemi4.py
@@ -4,13 +4,10 @@ arr = []
 for i in range(size):
     arr.append(int(input("Enter value {}:".format(i + 1))))
 
-if size > 0:
-    highest = float("-inf")
+highest = float("-inf")
 
-    for x in range(1, len(arr)):
-        if arr[x] > highest:
-            highest = arr[x]
+for x in range(1, len(arr)):
+    if arr[x] > highest:
+        highest = arr[x]
 
-    print("{} is the highest".format(highest))
-else:
-    print("The list is empty!")
+print("{} is the highest".format(highest))
